86003e65215cf76a757a7e2d4aaf677197c582be
Dave Spano <dspano@spano.us>
2018-02-27 01:03:54 -0500

    Update constants

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,3 @@
-STATUS = {'completed': 0, 'error': 1, 'started': 2, 'pending': 3,
-          'received': 4}
+STATUS = {0: 'completed', 1: 'error', 2: 'started', 3: 'pending',
+          4: 'received'}
 STATUS_FILE_PATH = '/tmp/backup_status/status.db'
